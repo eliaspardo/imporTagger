92b402555f1371b4bf273664aefbca24e58cb2ae
Elias <elias@ybvr.com>
2024-06-13 23:43:16 +0200

    Downloading .zip, unzipping. Need to parse and delete resulting files. Very much WIP.

--- src/main/kotlin/Repository.kt
+++ src/main/kotlin/Repository.kt
@@ -17,8 +17,10 @@ import io.ktor.client.request.forms.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.json.Json
-import org.slf4j.LoggerFactory
+import java.io.File
+import java.util.zip.ZipFile
 
 suspend fun logInOnXRay(xrayClientID:String, xrayClientSecret:String): HttpStatusCode {
     val client = HttpClient(CIO)
@@ -84,11 +86,6 @@ suspend fun importFileToXray(path: String): HttpStatusCode {
     importResponseMessage = response.status.description
     importResponseBody = response.body()
 
-    println(importResponseCode)
-    println(importResponseMessage)
-    println(importResponseBody.toString())
-
-
     // TODO Continue parsing response body
     if(!importResponseBody.errors.isEmpty()){
         println("There are errors")
@@ -100,5 +97,61 @@ suspend fun importFileToXray(path: String): HttpStatusCode {
 
     client.close()
     return response.status
+}
+
+suspend fun downloadCucumberTestsFromXRay(): File {
+    val client = HttpClient(CIO) {
+        install(Logging) {
+            logger = Logger.DEFAULT
+            level = LogLevel.ALL
+        }
+        install(Auth) {
+            bearer {
+                loadTokens {
+                    BearerTokens(loginToken, "")
+                }
+            }
+        }
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            })
+        }
+    }
+    val file = File.createTempFile("xrayImporter", ".zip")
+    runBlocking {
+        val httpResponse: HttpResponse = client.get("https://xray.cloud.getxray.app/api/v2/export/cucumber?keys=TEST-2806") {
+            onDownload { bytesSentTotal, contentLength ->
+                println("Received $bytesSentTotal bytes from $contentLength")
+            }
+        }
+        val responseBody: ByteArray = httpResponse.body()
+        file.writeBytes(responseBody)
+        println("A file saved to ${file.path}")
+        println(httpResponse.status.value)
+        println(httpResponse.status.description)
+    }
+    client.close()
+    return file.absoluteFile
+}
+
+fun unzipFile(file:File){
+    ZipFile(file).use { zip ->
+        zip.entries().asSequence().forEach { entry ->
+            zip.getInputStream(entry).use { input ->
+                File(file.absolutePath+entry.name).outputStream().use { output ->
+                    input.copyTo(output)
+                }
+            }
+        }
+    }
+}
+// TODO for test purposes only
+suspend fun main(args: Array<String>) {
+    logInOnXRay();
+    var file = downloadCucumberTestsFromXRay()
+    unzipFile(file)
+    // TODO Remove ZIP files and extracted files
 }
 
